b1978a9342d65ba21cb2503a6d5f05c712140fa1
j.shirkhani <j.shirkhani@fanap.ir>
2025-12-17 20:59:00 +0330

    initial commit

---
+++ src/main/java/com/jash/pm/simplegraphql/SimpleGraphQlApplication.java
@@ -0,0 +1,13 @@
+package com.jash.pm.simplegraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleGraphQlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleGraphQlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jash/pm/simplegraphql/SimpleGraphQlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jash.pm.simplegraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleGraphQlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
